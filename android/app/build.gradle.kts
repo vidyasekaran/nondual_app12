430831afd7efed486c20a2da50cde96157a38077
ITBS <harinehlc@gmail.com>
2026-03-31 11:11:50 +0530

    versionCode increased

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "3.1.0"
     }
     
